289f0579a81554f56569f5ff938dc54fd235b25a
kumar siddharatha <k.siddharatha@gmail.com>
2019-09-27 21:07:51 +0530

    removed duplicates in the result

--- CSVORM_Magnitude/Controllers/ValuesController.cs
+++ CSVORM_Magnitude/Controllers/ValuesController.cs
@@ -5,8 +5,10 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Web;
 using System.Web.Http;
+using Microsoft.CSharp.RuntimeBinder;
 using Microsoft.VisualBasic.FileIO;
 
 namespace CSVORM_Magnitude.Controllers
@@ -24,6 +26,7 @@ namespace CSVORM_Magnitude.Controllers
         {
             return _values.Keys;
         }
+  
         public override bool TryGetMember(GetMemberBinder binder, out object result)
         {
             if (_values.ContainsKey(binder.Name))
@@ -34,6 +37,7 @@ namespace CSVORM_Magnitude.Controllers
             result = null;
             return false;
         }
+
     }
     public enum AndOrOr { AND,OR};
     public class SimpleConditions : IRowMatch
@@ -62,27 +66,22 @@ namespace CSVORM_Magnitude.Controllers
         void isRowMatch(string[] returnRow, string[] conditions);
     }
 
-
     public class ValuesController : ApiController
     {
-        HashSet<DynamicEntity> dynPosts = new HashSet<DynamicEntity>();
-
+        
         // select name from test.csv where salary > 30
         // GET api/values
         public IEnumerable<DynamicEntity> Get(string csvTable)
         {
+            List<DynamicEntity> dynPosts = new List<DynamicEntity>();
             var path = HttpContext.Current.Server.MapPath(@"~\App_Data\" + csvTable + ".csv");
             var conditionClauses = "(salary < 4000000) OR (employee = TRUE)";
             string[] conditionClause;
             string[] complexCondition = conditionClauses.Split(new string[] { "AND", "OR" }, StringSplitOptions.RemoveEmptyEntries);
 
             ComplexCondition complex = new ComplexCondition();
-            if (conditionClauses.Contains("AND"))
-            {
-                complex.AndOrOr = AndOrOr.AND;
-            } else {
-                complex.AndOrOr = AndOrOr.OR;
-            }
+            complex.AndOrOr = conditionClauses.Contains("AND") ? AndOrOr.AND : AndOrOr.OR;
+
             complex.condtions = new List<SimpleConditions>();
 
             foreach (string condition in complexCondition) {
@@ -111,15 +110,15 @@ namespace CSVORM_Magnitude.Controllers
                     }
                 }
             }
+            
             return dynPosts;
 
         }
 
-        public void rowFinder(string[] row,string[] fields, SimpleConditions condition, ref HashSet<DynamicEntity> dynPosts)
+        public void rowFinder(string[] row,string[] fields, SimpleConditions condition, ref List<DynamicEntity> dynPosts)
         {
             string[] returnRow = row;
-            int[] selectIndex = { Array.IndexOf(fields, "name"), Array.IndexOf(fields, "salary") };
-
+            int[] selectIndex = { Array.IndexOf(fields, "emp_id") ,Array.IndexOf(fields, "name"), Array.IndexOf(fields, "salary") };
             int conditionIndex = Array.IndexOf(fields, condition.colName);
             string whereClause = condition.value;
             switch (condition.opera)
@@ -128,12 +127,7 @@ namespace CSVORM_Magnitude.Controllers
                     {
                         if (whereClause.Equals(returnRow[conditionIndex]))
                         {
-                            var entry = addToList(selectIndex, fields, returnRow);
-                            if (entry != null)
-                            {
-                                dynPosts.Add(entry);
-                            }
-
+                            addToList(selectIndex, fields, returnRow, ref dynPosts);
                         }
                     }
                     break;
@@ -141,12 +135,7 @@ namespace CSVORM_Magnitude.Controllers
                     {
                         if (!whereClause.Equals(returnRow[conditionIndex]))
                         {
-                            var entry = addToList(selectIndex, fields, returnRow);
-                            if (entry != null)
-                            {
-                                dynPosts.Add(entry);
-                            }
-
+                            addToList(selectIndex, fields, returnRow, ref dynPosts);
                         }
                     }
                     break;
@@ -156,11 +145,7 @@ namespace CSVORM_Magnitude.Controllers
                             && int.TryParse(whereClause, out int J)
                             && K < J)
                         {
-                            var entry = addToList(selectIndex, fields, returnRow);
-                            if (entry != null)
-                            {
-                                dynPosts.Add(entry);
-                            }
+                            addToList(selectIndex, fields, returnRow, ref dynPosts);
                         }
                     }
                     break;
@@ -170,11 +155,8 @@ namespace CSVORM_Magnitude.Controllers
                             && int.TryParse(whereClause, out int J)
                             && K > J)
                         {
-                            var entry = addToList(selectIndex, fields, returnRow);
-                            if (entry != null)
-                            {
-                                dynPosts.Add(entry);
-                            }
+                            addToList(selectIndex, fields, returnRow, ref dynPosts);
+                            
                         }
                     }
                     break;
@@ -184,11 +166,7 @@ namespace CSVORM_Magnitude.Controllers
                             && int.TryParse(whereClause, out int J)
                             && K >= J)
                         {
-                            var entry = addToList(selectIndex, fields, returnRow);
-                            if (entry != null)
-                            {
-                                dynPosts.Add(entry);
-                            }
+                            addToList(selectIndex, fields, returnRow, ref dynPosts);
                         }
                     }
                     break;
@@ -198,27 +176,45 @@ namespace CSVORM_Magnitude.Controllers
                             && int.TryParse(whereClause, out int J)
                             && K <= J)
                         {
-                            var entry = addToList(selectIndex, fields, returnRow);
-                            if (entry != null)
-                            {
-                                dynPosts.Add(entry);
-                            }
+                          addToList(selectIndex, fields, returnRow, ref dynPosts);
                         }
                     }
                     break;
             }
         }
+        private static object GetProperty(dynamic target, string name)
+        {
+            var site =
+                CallSite<Func<CallSite, dynamic, object>>
+                  .Create(Microsoft.CSharp.RuntimeBinder.Binder.GetMember(CSharpBinderFlags.None, name, target.GetType(),
+                        new[] { CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null) }));
+            return site.Target(site, target);
+        }
 
-        public DynamicEntity addToList(int[] selectIndex, string[] fields, string[] returnRow)
+        public int LookUpAlt(List<DynamicEntity> lst, string propName, object value)
+        {
+            if (lst.Count == 0) {
+                return 0;
+            }
+            var values = lst.FindAll(i => GetProperty(i, propName).Equals(value));
+            return (values).Count;
+        }
+        public object LookUp(List<dynamic> lst, string propName, object value)
+        {
+            return lst.FindAll(i => GetProperty(i, propName).Equals(value));
+        }
+        public void addToList(int[] selectIndex, string[] fields, string[] returnRow,ref List<DynamicEntity> dynPosts)
         {
             Dictionary<string, object> values = new Dictionary<string, object>();
 
-            foreach (int i in selectIndex)
+            if (LookUpAlt(dynPosts,"emp_id",returnRow[0]) == 0)
             {
-                values.Add(fields[i], returnRow[i]);
+                foreach (int i in selectIndex)
+                {
+                    values.Add(fields[i], returnRow[i]);
+                }
+                dynPosts.Add(new DynamicEntity(values));
             }
-            return new DynamicEntity(values);
-
         }
 
         public string[] csvParserDefaultSet(TextFieldParser csvParser)
